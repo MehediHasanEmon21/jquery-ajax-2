6458cc36cd0bf46e3004c21130f16260354994af
MehediHasanEmon21 <mh.emon2021@gmail.com>
2020-10-05 14:38:32 +0600

    advance product filter

--- app/Http/Controllers/AjaxController.php
+++ app/Http/Controllers/AjaxController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 
 use App\AjaxCrud;
 use App\DynamicField;
+use App\ProductFilter;
 use DB;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\URL;
@@ -12,43 +13,110 @@ use Illuminate\Support\Facades\Validator;
 class AjaxController extends Controller
 {
     public function index(){
+
+
         
-        // $products = DB::table('products')->limit()->orderBy('id','DESC')->get();
-        // $url  = URL::to('/');
-        // dd($url);
-        return view('index');
+        $data['products'] = DB::table('product_filter')->get();
+        $data['product_ram'] = DB::table('product_filter')->select('product_ram')->groupBy('product_ram')->get();
+        $data['product_brand'] = DB::table('product_filter')->select('product_brand')->groupBy('product_brand')->get();
+        $data['product_storage'] = DB::table('product_filter')->select('product_storage')->groupBy('product_storage')->get();
+        $data['min_price'] = DB::table('product_filter')->min('product_price');
+        $data['max_price'] = DB::table('product_filter')->max('product_price');
+     
+        return view('index',$data);
 
     }
 
     public function fetch(Request $request){
 
-        
-        // $url 
-        $term = $request->term;
+      
 
-        $products = DB::table('products')->where('product_name','LIKE', '%'.$term.'%')->orderBy('product_name','ASC')->get();
 
-        $output = array();
-         if($products->count() > 0)
-         {
-          foreach($products as $row)
-          {
-           $temp_array = array();
-           $temp_array['value'] = $row->product_name;
-           $temp_array['id'] = $row->id;
-           $temp_array['label'] = '<img src="'.URL::to('/').'/'.$row->image_one.'" width="70" />&nbsp;&nbsp;&nbsp;'.$row->product_name.'';
-           $output[] = $temp_array;
+       
+       $products = ProductFilter::where(function($query) use ($request){
+
+       
+
+        if(isset($request->minimum_price) && isset($request->maximum_price)){
+
+          if (isset($request->brand)) {
+
+            
+             $query->whereIn('product_brand',$request->brand);
+             $query->where('product_price','>=',$request->minimum_price);
+             $query->where('product_price','<=',$request->maximum_price);
+          
+
+            
           }
-         }
-         else
-         {
-          $output['value'] = '';
-          $output['label'] = 'No Record Found';
-         }
 
-         echo json_encode($output);
-              
+          if (isset($request->ram)) {
 
+            
+             $query->whereIn('product_ram',$request->ram);
+             $query->where('product_price','>=',$request->minimum_price);
+             $query->where('product_price','<=',$request->maximum_price);
+          
+
+            
+          }
+
+          if (isset($request->storage)) {
+
+            
+             $query->whereIn('product_storage',$request->storage);
+             $query->where('product_price','>=',$request->minimum_price);
+             $query->where('product_price','<=',$request->maximum_price);
+          
+
+            
+          }
+
+
+          $query->where('product_price','>=',$request->minimum_price);
+          $query->where('product_price','<=',$request->maximum_price);
+        }
+
+      })->get();
+
+       $output = '';
+
+       if (count($products) > 0) {
+         foreach($products as $row)
+        {
+          $output .= '
+          <div class="col-sm-4 col-lg-3 col-md-3">
+            <div style="border:1px solid #ccc; border-radius:5px; padding:16px; margin-bottom:16px; height:450px;">
+              <img src="/filter/img/'. $row->product_image .'" alt="" class="img-responsive" >
+              <p align="center"><strong><a href="#">'. $row->product_name .'</a></strong></p>
+              <h4 style="text-align:center;" class="text-danger" >'. $row->product_price.'</h4>
+              <p>Camera : '. $row->product_camera.' MP<br />
+              Brand : '. $row->product_brand .' <br />
+              RAM : '. $row->product_ram .' GB<br />
+              Storage : '. $row->product_storage .' GB </p>
+            </div>
+
+          </div>
+          ';
+        }
+       }else{
+          $output = '<h3>No Data Found</h3>';
+       }
+
+       echo $output;
+       
+
+        
+
+        
+
+       
+
+
+
+
+
+       
     }
 
 
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,66 +1,178 @@
+
+
 <!DOCTYPE html>
-<html>
-  <head>
-    <title>jQuery UI Autocomplete with Images and Custom HTML in PHP</title>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.js"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.css" />
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<html lang="en">
 
-    <style type="text/css">
-      .ui-autocomplete-row
-      {
-        padding:8px;
-        background-color: #f4f4f4;
-        border-bottom:1px solid #ccc;
-        font-weight:bold;
-      }
-      .ui-autocomplete-row:hover
-      {
-        background-color: #ddd;
-      }
-    </style>
-  </head>
-  <body>
-    <br />
-    <br />
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Product filter in php</title>
+
+    <script src="{{asset('filter')}}/js/jquery-1.10.2.min.js"></script>
+    <script src="{{asset('filter')}}/js/jquery-ui.js"></script>
+    <script src="{{asset('filter')}}/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="{{asset('filter')}}/css/bootstrap.min.css">
+    <link href = "{{asset('filter')}}/css/jquery-ui.css" rel = "stylesheet">
+    <!-- Custom CSS -->
+    <link href="{{asset('filter')}}/css/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <!-- Page Content -->
     <div class="container">
-      <h3 align="center">Ajax AutoComplete Textbox with Image using jQuery UI in PHP</h3>
-      <br />
-      <br />
-      <br />
-      <div class="row">
-        <div class="col-md-3">
+        <div class="row">
+          <br />
+          <h2 align="center">Advance Ajax Product Filters in PHP</h2>
+          <br />
+            <div class="col-md-3">                        
+        <div class="list-group">
+          <h3>Price</h3>
+                    <input type="hidden" id="hidden_minimum_price" value="{{intval($min_price)}}" />
+                    <input type="hidden" id="hidden_maximum_price" value="{{intval($max_price)}}" />
+                    <p id="price_show">{{intval($min_price)}} - {{intval($max_price)}}</p>
+                    <div id="price_range"></div>
+                </div>        
+                <div class="list-group">
+          <h3>Brand</h3>
+                    <div style="height: 180px; overflow-y: auto; overflow-x: hidden;">
+                  @foreach($product_brand as $brand)
+                    <div class="list-group-item checkbox">
+                        <label><input type="checkbox" class="common_selector brand" value="{{$brand->product_brand}}"  > {{$brand->product_brand}}</label>
+                    </div>
+                   @endforeach
+                    </div>
+                </div>
 
-        </div>
-        <div class="col-md-6">
-          <input type="text" id="search_data" placeholder="Enter Student name..." autocomplete="off" class="form-control input-lg" />
-        </div>
-        <div class="col-md-3">
+        <div class="list-group">
+          <h3>RAM</h3>
+                    @foreach($product_ram as $row)
+                    <div class="list-group-item checkbox">
+                        <label><input type="checkbox" class="common_selector ram" value="{{$row->product_ram}}" > {{$row->product_ram}} GB</label>
+                    </div>
+                    @endforeach
+                </div>
+        
+        <div class="list-group">
+          <h3>Internal Storage</h3>
+  
+                    @foreach($product_storage as $storage)
+                    <div class="list-group-item checkbox">
+                        <label><input type="checkbox" class="common_selector storage" value="{{$storage->product_storage}}"  > {{$storage->product_storage}} GB</label>
+                    </div>
+                    @endforeach
+                </div>
+            </div>
 
+            <div class="col-md-9">
+              <br />
+                <div class="row filter_data">
+                  @foreach($products as $product)
+                  <div class="col-sm-4 col-lg-3 col-md-3">
+                      <div style="border:1px solid #ccc; border-radius:5px; padding:16px; margin-bottom:16px; height:450px;">
+                        <img src="/filter/img/{{ $product->product_image }}" alt="" class="img-responsive" >
+                        <p align="center"><strong><a href="#">{{$product->product_name}}</a></strong></p>
+                        <h4 style="text-align:center;" class="text-danger" >{{$product->product_price}}</h4>
+                        <p>Camera : {{$product->product_camera}} MP<br />
+                        Brand : {{$product->product_brand}} <br />
+                        RAM : {{$product->product_ram}} GB<br />
+                        Storage : {{$product->product_storage}} GB </p>
+                      </div>
+
+                    </div>
+                    @endforeach
+
+                </div>
+            </div>
         </div>
-      </div>
+
     </div>
-  </body>
-</html>
-<script>
-  $(document).ready(function(){
-      
-    $('#search_data').autocomplete({
-      source: "api/fetch",
-      minLength: 1,
-      select: function(event, ui)
-      { 
-        console.log(ui)
-        $('#search_data').val(ui.item.value);
-      }
-    }).data('ui-autocomplete')._renderItem = function(ul, item){
-      return $("<li class='ui-autocomplete-row'></li>")
-        .data("item.autocomplete", item)
-        .append(item.label)
-        .appendTo(ul);
-    };
 
-  });
-</script>
+     <style>
+        #loading {
+            text-align: center;
+            background: url('loader.gif') no-repeat center;
+            height: 150px;
+        }
+    </style>
+
+
+
+    <script type="text/javascript">
+      
+      $(function(){
+
+            var min_price = parseInt($('#hidden_minimum_price').val());
+            var max_price = parseInt($('#hidden_maximum_price').val());
+            
+            $('#price_range').slider({
+              range:true,
+              min:min_price,
+              max:max_price,
+              values:[min_price, max_price],
+              step:500,
+              stop:function(event, ui)
+              {   console.log(ui)
+                  $('#price_show').html(ui.values[0] + ' - ' + ui.values[1]);
+                  $('#hidden_minimum_price').val(ui.values[0]);
+                  $('#hidden_maximum_price').val(ui.values[1]);
+                  filter_data()
+                  
+              }
+          });
+
+
+          function get_filter(class_name) {
+                var filter = [];
+                $('.' + class_name + ':checked').each(function() {
+                    filter.push($(this).val());
+                });
+                return filter;
+            }
+
+            $('.common_selector').click(function() {
+                filter_data();
+            });
+
+
+              function filter_data() {
+                $('.filter_data').html('<div id="loading" style="" ></div>');
+                var action = 'fetch_data';
+                var minimum_price = $('#hidden_minimum_price').val();
+                var maximum_price = $('#hidden_maximum_price').val();
+                var brand = get_filter('brand');
+                var ram = get_filter('ram');
+                var storage = get_filter('storage');
+                $.ajax({
+                    url: "/api/fetch",
+                    method: "POST",
+                    data: {
+                        action: action,
+                        minimum_price: minimum_price,
+                        maximum_price: maximum_price,
+                        brand: brand,
+                        ram: ram,
+                        storage: storage
+                    },
+                    success: function(data) {
+                      $('.filter_data').html(data);
+                    }
+                });
+            }
+
+
+      })
+
+    </script>
+
+
+
+
+
+</body>
+
+</html>
